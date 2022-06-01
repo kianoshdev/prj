812e182409a13c712e83d932195d273598fb3c8c
kianosh iv <kianoshiv1733@gmail.com>
2022-06-01 19:57:46 +0430

    FinalVersion

--- Findnumber/Program.cs
+++ Findnumber/Program.cs
@@ -25,35 +25,43 @@ namespace ConsoleApp8
             string input2 = Console.ReadLine();
 
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("{0} is {1} time repeat",input2,costomcontain(input1, input2));
+            Console.WriteLine("{0} is {1} time repeat", input2, costomcontain(input1, input2));
             Console.ResetColor();
         }
         public static int costomcontain(string sen, string wo)
         {
-            
+
             bool flag = false;
             int Countnum = 0;
-            int helpint = 0;
+            int n = 0;
 
-            for (int n = 0; n <= sen.Length - 1; n++)
+            // for (int n = 0; n <= sen.Length - 1; n++)
+            // {
+            // helpint = n;
+            for (int i = 0; i <= wo.Length - 1; i++)
             {
-                helpint = n;
-                for (int i = 0; i <= wo.Length - 1; i++)
+                if (n == sen.Length) break;
+                if (wo[i] == sen[n])
                 {
-                    if (wo[i] == sen[helpint])
-                    {
-                        helpint++;
-                        flag = true;
-                    }
-                    else
-                    {
-                        i--;
-                        flag = false;
-                        break;
-                    }
+                    n++;
+                    flag = true;
                 }
-                Countnum = flag ? Countnum += 1 : Countnum += 0;
+                else
+                {
+                    i = -1;
+                    n++;
+                    flag = false;
+                }
+                if (i == wo.Length - 1)
+                {
+                    Countnum++;
+                    i = -1;
+                    n++;
+                    continue;
+                }
             }
+            // Countnum = flag ? Countnum += 1 : Countnum += 0;
+            //}
             return Countnum;
         }
     }
